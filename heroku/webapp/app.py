f40489c4fabcb0dde27d21fb93aee9d4074e66c4
oort77 <73858914+oort77@users.noreply.github.com>
2022-01-05 06:32:33 +0300

    ..

--- heroku/webapp/app.py
+++ heroku/webapp/app.py
@@ -65,7 +65,7 @@ with st.sidebar.expander("I want to choose my values", expanded=False):
 
 send_req = st.button('Send get request')
 
-worker_port = int(os.environ.get("PORT", 8080))
+worker_port = int(os.environ.get("WPORT", 8080))
 worker_address = "http://worker:" + str(worker_port) + "/predict/"
 
 # Main page button
